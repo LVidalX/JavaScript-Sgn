fef37e97172ba73608051eb5f14515904316be54
LVidalX <luisvidalpuma@gmail.com>
2023-11-21 08:18:13 -0500

    Tarea-validarTaxi-Semana7

--- Tareas/Semana7/validarTaxi/Ejercicio1.js
+++ Tareas/Semana7/validarTaxi/Ejercicio1.js
@@ -1,2 +1,32 @@
+'use strict'
 
+let vehicle = {
+    make: "Hyundai",
+    model: 'Compact',
+    color: 'Yellow',
+    licensePlate: 'NYK-524',
+    year: 2015,
+    isEnable: true
+}
+
+/*Crear una funcion 'validarTaxi' que reciba como parametro el objeto vehicle y devuelva true si la antiguedad 
+del vehiculo es menor o igual a 10 años, y false si es mas.
+*/
+
+function validarTaxi(año) {
+    let añoActual = 2023
+    let Antiguedad
+    
+    Antiguedad =(añoActual - año)
+
+    if(Antiguedad <= 10) {
+        Antiguedad = true
+        alert("Antiguedad: "+ Antiguedad) 
+    } else {
+        Antiguedad = false
+        alert("Antiguedad: "+Antiguedad)
+    }
+}
+
+validarTaxi(vehicle.year)
 
