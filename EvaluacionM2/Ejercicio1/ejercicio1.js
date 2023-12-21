40823ffdd3bc55451f1ac4f200f1a2fa7b499542
LVidalX <luisvidalpuma@gmail.com>
2023-12-21 10:26:18 -0500

    EvaluacionM2

--- EvaluacionM2/Ejercicio1/ejercicio1.js
+++ EvaluacionM2/Ejercicio1/ejercicio1.js
@@ -9,4 +9,36 @@ Una Función obtenerUsuario que devuelva únicamente el nombre de usuario del em
  Crear un objeto a partir de la función anterior y comprobar funciones 
 */
 
+function Estudiante(nombre, correo, promedio){
+    this.nombre = nombre;
+    this.correo = correo;
+    this.promedio = promedio;
+}
+
+
+let estudiante_1 = new Estudiante("Jose", "josejk35@gmail.com", "85")
+
+let promedio = (estudiante_1.promedio);
+let correo = (estudiante_1.correo);
+
+function validarPromedio(promedio){
+     promedio = (estudiante_1.promedio);
+
+    if(promedio > 70){
+        alert("Aprobado");
+     } else {
+        alert("Reprobado");
+    }
+    return promedio;
+}
+
+validarPromedio(promedio);
+
+alert(correo);
+
+let pos = (correo.indexOF("<"));
+
+usuario = (correo)
+
+
 
